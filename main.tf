4f210a367c8165fd36117e88196fa51432cdf6ff
siddharthg <siddharthdg857@gmail.com>
2024-06-16 23:22:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket" "sample" {
 	project	= "esoteric-source-422613-j4"
 	location = "EU"
 }
+
+resource "google_storage_bucket" "sample" {
+	name = "sid-bucket-160624-2"
+	project	= "esoteric-source-422613-j4"
+	location = "EU"
+}
